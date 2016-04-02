3cbb3f5e6df6c114b70313bddece224e087fe02a
Cagatay Gurturk <cagatay@itusozluk.com>
2016-04-02 12:55:01 +0200

    Vagrant change

--- puppet/modules/scripts/files/post.sh
+++ puppet/modules/scripts/files/post.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 mysql -u root -e "GRANT ALL ON *.* to 'dev'@'%' identified by 'ved';FLUSH PRIVILEGES;CREATE DATABASE keinewaste;"
+touch /var/log/keinewaste.log && chmod 0777 /var/log/keinewaste.log
